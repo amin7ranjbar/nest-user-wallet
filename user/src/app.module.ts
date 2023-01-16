a9e13e9b1e04c51fd23ff07f6ed1bea7083c6430
amin7ranjbar <amin7ranjbar@gmail.com>
2023-01-16 09:40:43 +0330

    add user service

--- user/src/app.module.ts
+++ user/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from "@nestjs/config";
+import { UserModule } from './user/user.module';
+import { RmqModule } from "nest-rabbitmq";
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dataSource } from './config';
+import { UserEntity } from './entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    RmqModule.register({
+      uri: process.env.RABBITMQ_URL,
+    }),
+    TypeOrmModule.forRoot(dataSource.options),
+    TypeOrmModule.forFeature([UserEntity]),
+    UserModule
+  ]
 })
 export class AppModule {}
